30febb4b9f5c12418b943da9c7af9131b28c2293
orellachichester@gmail.com <orellachichester@gmail.com>
2022-01-28 13:35:38 -0500

    Installed react-router-dom/Linked Pages to Nav and App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+
 //Components
+import Nav from "./components/Nav"
 //Pages
+import About from "./pages/About"
+import Cart from "./pages/Cart"
+import Coupon from "./pages/Coupon"
+import Product from "./pages/Product"
+import Home from "./pages/Home"
 //Contexts
+import UserContext from "./contexts/UserContext"
 //CSS
+import "./App.css"
 
 
 const App = () => {
   return (
+
     <div>
+      < Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="coupon" element={<Coupon />} />
+        <Route path="product" element={<Product />} />
       
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            ABOUT US
+        </div>
+    );
+}
+
+export default Index;
