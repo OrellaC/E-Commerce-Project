c226d2140fb99fe690202c5a56a6f5b468764d73
orellachichester@gmail.com <orellachichester@gmail.com>
2022-01-29 22:31:22 -0500

    Added Bootstrap card

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -3,13 +3,17 @@ import React from 'react';
 const Product = ({ itemList }) => {
     console.log(itemList)
 
-    
+    //Key prop is needed for each child list
+    const items = itemList.map((items, itemKey) => <li key={itemKey}> {items.title} </li>)
+
     return (
-        <div>
-            Shop Our Latest Items!
-            {
-                itemList.map(items => <li> {items.title} </li>)
-            }
+        <div className="card" style={{width: "18rem;"}}>
+            {/* <img src="..." className="card-img-top" alt="..." /> */}
+                <div className="card-body">
+                    <h5 className="card-title">{items.title}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
         </div>
     );
 }
