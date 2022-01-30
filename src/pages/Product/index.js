edb1998ff26fbd8035532d67ff923b6d2f1162b9
orellachichester@gmail.com <orellachichester@gmail.com>
2022-01-29 23:41:44 -0500

    Rendering product names

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -4,17 +4,21 @@ const Product = ({ itemList }) => {
     console.log(itemList)
 
     //Key prop is needed for each child list
-    const items = itemList.map((items, itemKey) => <li key={itemKey}> {items.title} </li>)
+    const items = itemList.map((items, i) => <li key={i}>{items.title}</li> )
 
     return (
-        <div className="card" style={{width: "18rem;"}}>
-            {/* <img src="..." className="card-img-top" alt="..." /> */}
+        <div className="card" style={{width: "18rem"}} >
+         <img src="..." className="card-img-top" alt="..." />
+         {/* {itemList.map(items =>  items.title )} */}
                 <div className="card-body">
-                    <h5 className="card-title">{items.title}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 className="card-title"> Title:{items.title} </h5>
+                    <p className="card-text">Price: {items.price} </p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
+                </div> 
         </div>
+    
+           
+        
     );
 }
 
