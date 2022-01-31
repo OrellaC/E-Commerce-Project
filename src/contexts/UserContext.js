56c9a72e686eb77584e14c54e3295fbd867c98ab
orellachichester@gmail.com <orellachichester@gmail.com>
2022-01-30 22:27:35 -0500

    Updated User Context Page

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,6 +1,12 @@
+//import createContext
+
 import React, { createContext } from 'react'
 
-const Usercontext = createConext(null)
 
+//simplest way to create a context is by creating a variable similar to the name of the file
+//Standard Practice: Name your context the same as your file
+//useContext is just a different way of passing on data 
 
-export default Usercontext;
+const UserContext = createContext(null)
+
+export default UserContext 
