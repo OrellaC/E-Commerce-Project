317ef669703b785ccf007dbad23866abd9706024
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-01 23:08:41 -0500

    IncrementItem/DecreaseItem

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,38 +1,62 @@
 import React from 'react';
 
-const removeFromCart = () =>{
-    
-}
+// const removeFromCart = () => {
 
-const quantity = () =>{
+// }
 
-}
-const Cart = ({cart}) => {
+// const quantity = () => {
+//     // clicks: 0,
+//     //     show: true
+// };
+
+// IncrementItem = () => {
+//     quantity.setState({ clicks: quantity.state.clicks + 1 });
+// }
+// DecreaseItem = () => {
+//     quantity.setState({ clicks: quantity.state.clicks - 1 });
+// }
+// ToggleClick = () => {
+//     quantity.setState({ show: !quantity.state.show });
+// }
+
+
+
+const Cart = ({ cart }) => {
     console.log(cart)
     return (
         <div className="product-container">
             <h1>My Cart</h1>
 
-        {
-            cart && cart.map((items) => {
-                console.log(items)
-                return (
-                    <div className="card" style={{ width: "18rem" }} key={items.title} >
-                        <img src={items?.image} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title"> {items.title} </h5>
-                            <p className="card-text">${items.price} </p>
-                            <button className="btn btn-primary" onClick={()=>removeFromCart()}> Remove </button>
+            {
+                cart && cart.map((items) => {
+                    console.log(items)
+                    return (
+                        <div className="card" style={{ width: "18rem" }} key={items.title} >
+                            <img src={items?.image} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title"> {items.title} </h5>
+                                <p className="card-text">${items.price} </p>
+                                <button className="btn btn-primary"> Remove </button>
+                               
+                               
+                               
+                                {/* Adjusts quantity of item */}
+                                {/* <button onClick={quantity.IncrementItem}>Click to increment by 1</button>
+                                <button onClick={quantity.DecreaseItem}>Click to decrease by 1</button>
+                                <button onClick={quantity.ToggleClick}>
+                                    {quantity.state.show ? 'Hide number' : 'Show number'}
+                                </button>
+                                {quantity.state.show ? <h2>{quantity.state.clicks}</h2> : ''} */}
+                            </div>
                         </div>
-                    </div>
-            
-                )
-            })
-        }
-        <div>
-           <h4>Subtotal:</h4>
-           </div>
-    </div>
+
+                    )
+                })
+            }
+            <div>
+                <h4>Subtotal:</h4>
+            </div>
+        </div>
     );
 }
 
