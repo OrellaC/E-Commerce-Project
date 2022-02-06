4cd4ebefab1732029b1e51cae39f403f1d85bdee
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-05 23:04:45 -0500

    Created and added a banner on cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Product from '../Product';
+import cartImage from "./Images/cartImage.png"
 
 
 
@@ -7,7 +8,11 @@ const Cart = ({ cart, removeFromCart }) => {
     console.log(cart)
     return (
         <div className="product-container">
-            <h1>My Cart</h1>
+            <div className='product-child'>
+
+           
+            <img src={cartImage} />
+          
 
             {
                 cart && cart.map((items) => {
@@ -19,26 +24,29 @@ const Cart = ({ cart, removeFromCart }) => {
                                 <h5 className="card-title"> {items.title} </h5>
                                 <p className="card-text">${items.price} </p>
                                 <button className="btn btn-primary" onClick={()=>removeFromCart(items)}> Remove </button>
+                                </div>
                                
-                               
-                               
-                                {/* Adjusts quantity of item */}
-                                {/* <button onClick={quantity.IncrementItem}>Click to increment by 1</button>
+                                </div>
+
+                                /* Adjusts quantity of item */
+                                /* <button onClick={quantity.IncrementItem}>Click to increment by 1</button>
                                 <button onClick={quantity.DecreaseItem}>Click to decrease by 1</button>
                                 <button onClick={quantity.ToggleClick}>
                                     {quantity.state.show ? 'Hide number' : 'Show number'}
                                 </button>
-                                {quantity.state.show ? <h2>{quantity.state.clicks}</h2> : ''} */}
-                            </div>
-                        </div>
-
+                                {quantity.state.show ? <h2>{quantity.state.clicks}</h2> : ''} */  
                     )
                 })
             }
             <div>
                 <h4>Subtotal:</h4>
             </div>
+
+
+            </div>
+            {/* Closes Child container */}
         </div>
+        // Closes Product container ^
     );
 }
 
