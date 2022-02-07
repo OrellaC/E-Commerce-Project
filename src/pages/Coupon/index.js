6b2f3d54c755b6410fb70a4f75342fd1c4628261
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-07 14:37:12 -0500

    Updated bootstrap on coupon page

--- src/pages/Coupon/index.js
+++ src/pages/Coupon/index.js
@@ -3,7 +3,9 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import "./styles.css"
 import emailVer from "./Images/emailVer.png"
-
+import envelope from "./Images/envelope.png"
+import emailLogo from "./Images/emailLogo.jpeg"
+import emailTech from "./Images/emailTech.png"
 
 const Coupon = () => {
   const [userInput, setUserInput] = useState('')
@@ -56,14 +58,14 @@ const Coupon = () => {
               </div>
             </div>
           </div>
+        </div>
 
-          <div className="form-body">
+        {/* Bootstrap Card */}
+        <div className="card text-center">
+          <div className="card-body">
+            <h3 className="card-title">Email Verification</h3>
+            <p className="card-text">In order to receive 25% off your first order, you must subscribe to our mailing list by providing a valid email address. </p>
             {console.log(data)}
-            <div className='form-child'>
-            <div className='header'>
-              <h1>In order to receive 25% off your first order, you must subscribe to our mailing list by providing a valid email address.</h1>
-           
-
             <form onSubmit={(e) => {
               return handleSubmit(e)
             }}>
@@ -76,33 +78,45 @@ const Coupon = () => {
                 value={data?.userInput}
                 required
               />
-              <input type='submit' value='Submit' />
+              <input type='submit' value='Submit' className="btn btn-primary" />
             </form>
-            </div>
-            </div>
-
-
-
-            <div className="email-box">
-              <h3>This what will appear on the client side. Client's will be able to see which parameters of the API the user's email matches. If the user's email does not validate true for "Is this a webmail", they will not be able to view the coupon code until they enter a valid email address.</h3>
-              <h4>Email Address:{data?.email_address}</h4>
-              <h4>Is this email syntax valid? {String(data?.valid_syntax)}</h4>
-              <h4>Is this a spam email? {String(data?.spam)}</h4>
-              <h4> Is this a webmail? {String(data?.webmail)}</h4>
-            </div>
-
 
             <div className='coupon-holder'>
-
               <Link to="/coupon/code">
                 {data?.webmail ? <button type="button" className='coupon-bttn' onClick={() => ({ show: !show })}>Unlock Coupon Code</button > : null}
               </Link>
             </div>
-          </div>
 
+          </div>
         </div>
+
+        <div className="card mb-3 center border-primary" style={{ maxwidth: "540px" }}>
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={emailTech} className="img-fluid rounded-start" alt="..." />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">API Parameters</h5>
+                <h6 className="card-text">Email Address: {data?.email_address}</h6>
+                <p>Is this email syntax valid? {String(data?.valid_syntax)} </p>
+                <p className="card-text">Is this a spam email? {String(data?.spam)}</p>
+                <p className="card-text">Is this a webmail? {String(data?.webmail)}</p>
+
+                <div className="card-body text-center">
+                  <h5 className="card-title">This information will appear on the client's side.</h5>
+                  <p className="card-text"> Client's will be able to see which parameters of the API the user's email matches. </p>
+                  <p>If the user's email does not validate true for "Is this a webmail", they will not be able to view the coupon code until they enter a valid email address. </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     </div>
+
 
   );
 }
