a18033a9de2cf6fb09df8f7834b8656b90da2b6c
orellachichester@gmail.com <orellachichester@gmail.com>
2022-03-03 01:24:20 -0500

    Rendered wishlist data onto eishlist page

--- src/pages/Wishlist/index.js
+++ src/pages/Wishlist/index.js
@@ -1,9 +1,55 @@
+import axios from 'axios';
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 const Wishlist = () => {
+    const [wItems, setWItems] = useState([])
+
+    useEffect(() => {
+        fetchListItems()
+    }, [])
+
+    const fetchListItems = async () => {
+        try {
+
+            const response = await axios.get("http://localhost:8080/api/v1/checkallitems")
+
+            console.log(response)
+            setWItems(response.data)
+
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    console.log("These are the items on your wishlist ", wItems)
     return (
         <div>
-            
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Item Name</th>
+                        <th scope="col">Item Description</th>
+                        <th scope="col">ID Number</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        wItems.map(wItems => {
+                            return(
+                                <tr key ={wItems.id}>
+                                <td>{wItems.itemname}</td>
+                                <td>{wItems.itemdescription}</td>
+                                <td>{wItems.id}</td>
+                            </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+
         </div>
     );
 }
